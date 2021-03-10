77404a8b564ca5b72b9fcaf432ce6971a4c218f7
raf-abtasty <raphael@abtasty.com>
2021-03-10 16:58:05 +0100

    clean demo app

--- demo/src/main/java/com/abtasty/demo/DemoFlagship.java
+++ demo/src/main/java/com/abtasty/demo/DemoFlagship.java
@@ -1,77 +1,32 @@
 package com.abtasty.demo;
 
-import com.abtasty.flagship.hits.Event;
-import com.abtasty.flagship.hits.Page;
 import com.abtasty.flagship.hits.Screen;
 import com.abtasty.flagship.main.Flagship;
 import com.abtasty.flagship.main.FlagshipConfig;
 import com.abtasty.flagship.main.Visitor;
-import com.abtasty.flagship.model.Modification;
-import com.abtasty.flagship.utils.ILogManager;
-
-import java.util.HashMap;
-import java.util.logging.Level;
 
 public class DemoFlagship {
 
     public static void main(String[] args) {
 
 
-        class CustomLogManager extends ILogManager {
+        Flagship.start("my_env_id", "my api key", new FlagshipConfig());
 
-            @Override
-            public void onLog(Level level, String tag, String message) {
-                System.out.println("FLAGSHIP => " + tag + " " + message);
-            }
-        }
+        Visitor visitor = Flagship.newVisitor("visitor1");
 
-//        Flagship.start("my env id", "my api key", new FlagshipConfig());
-//        Flagship.start("bkk4s7gcmjcg07fke9dg", "Q6FDmj6F188nh75lhEato2MwoyXDS7y34VrAL4Aa", new FlagshipConfig()
-//                .withFlagshipMode(Flagship.Mode.DECISION_API)
-//                .withLogManager(new CustomLogManager())
-//                .withLogMode(ILogManager.LogMode.ALL)
-//                .withTimeout(2));
-//        Flagship.start("bkk4s7gcmjcg07fke9dg", "Q6FDmj6F188nh75lhEato2MwoyXDS7y34VrAL4Aa", new FlagshipConfig().withLogManager(new CustomLogManager(ILogManager.LogMode.ALL)));
-        Visitor visitor = Flagship.newVisitor("toto");
-        Visitor visitor2 = Flagship.newVisitor("toto2", new HashMap<String, Object>() {{
-            put("age", 31);
-            put("isVip", true);
-        }});
-        HashMap<String, Object> hashMap = new HashMap<String, Object>();
-//        hashMap.put("Age", new FlagshipConfig());
-        hashMap.put("Age", 31);
-        hashMap.put("isVIPUser", true);
-        hashMap.put("daysSinceLastLaunch", 2);
-        visitor.updateContext(hashMap);
-        visitor2.updateContext("lastPurchase", 1615384464);
-        visitor.updateContext("title", "Grand Mamamouchi", () -> {
-            System.out.println("Update context synchronized");
-        });
+        visitor.updateContext("isVIP", true);
+
         visitor.synchronizeModifications(() -> {
-            String value2 = visitor.getModification("isref", "default", false);
-            visitor.activateModification("isref");
-            int value3 = visitor.getModification("release", 0, true);
-            String value4 = visitor.getModification("title", "default", true);
-            int value5 = visitor.getModification("all_users", 0, true);
-            Modification m = new Modification("", "", "", "", true, null);
-            boolean value6 = visitor.getModification("featureEnabled", false, true);
-
-            System.out.println("Value 2 = " + value2.toString()); //
-            System.out.println("Value 3 = " + value3); //
-            System.out.println("Value 4 = " + value4.toString()); //
-            System.out.println("Value 5 = " + value5);
-            System.out.println("Value 6 = " + value6); //
-
-            visitor.sendHit(new Page("https://mydomain.com/java").withResolution(100, 100));
-            visitor.sendHit(new Screen("Page Java").withResolution(100, 100));
-            visitor.sendHit(new Event(Event.EventCategory.USER_ENGAGEMENT, "coucou").withResolution(2, 2).withEventLabel("label").withEventValue(666).withLocale("fr_FR"));
+            Boolean displayFeature = visitor.getModification("vipFeature", false, false);
+            System.out.println("displayFeature = " + displayFeature);
+            visitor.activateModification("vipFeature");
         });
+        visitor.sendHit(new Screen("main"));
+
         try {
-            Thread.sleep(10000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-//        Page p = new Page("www.java.com/version1");
     }
 }

92376713ac42fc64f81341cfac868796a065876f
raf-abtasty <raphael@abtasty.com>
2021-04-01 18:23:24 +0200

    update build gradle

--- demo/src/main/java/com/abtasty/demo/DemoFlagship.java
+++ demo/src/main/java/com/abtasty/demo/DemoFlagship.java
@@ -4,6 +4,7 @@ import com.abtasty.flagship.hits.Screen;
 import com.abtasty.flagship.main.Flagship;
 import com.abtasty.flagship.main.FlagshipConfig;
 import com.abtasty.flagship.main.Visitor;
+import com.abtasty.flagship.utils.LogManager;
 
 
 public class DemoFlagship {
@@ -11,7 +12,7 @@ public class DemoFlagship {
     public static void main(String[] args) {
 
 
-        Flagship.start("my_env_id", "my api key", new FlagshipConfig());
+        Flagship.start("my_env_id", "my api key", new FlagshipConfig().withLogLevel(LogManager.Level.ALL));
 
         Visitor visitor = Flagship.newVisitor("visitor1");
 

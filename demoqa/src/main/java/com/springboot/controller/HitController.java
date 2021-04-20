8d57f89a087efe164c8bf36d8d3a6a4f01fa0e43
raf-abtasty <raphael@abtasty.com>
2021-04-20 14:28:11 +0200

    re-enable Logs

--- demoqa/src/main/java/com/springboot/controller/HitController.java
+++ demoqa/src/main/java/com/springboot/controller/HitController.java
@@ -3,12 +3,10 @@ package com.springboot.controller;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.abtasty.flagship.hits.Event;
 import com.abtasty.flagship.hits.Item;
 import com.abtasty.flagship.hits.Page;

ce9e8ab3d977213940b4e308b8b374374dcd2d31
raf-abtasty <raphael@abtasty.com>
2021-04-26 14:46:26 +0200

    remove test fail

--- flagship/src/test/java/FlagshipIntegrationTests.java
+++ flagship/src/test/java/FlagshipIntegrationTests.java
@@ -131,7 +131,6 @@ public class FlagshipIntegrationTests {
 
     @Test
     public void config() {
-        assert false;
         Flagship.start(null, null, null);
         assertEquals(Flagship.getStatus(), Flagship.Status.NOT_READY);
 

39f5b4a0188054e069651636b5c1f4ccd07f734f
raf-abtasty <raphael@abtasty.com>
2021-09-08 18:24:51 +0200

    ok

--- demo/src/main/java/com/abtasty/demo/DemoFlagship.java
+++ demo/src/main/java/com/abtasty/demo/DemoFlagship.java
@@ -1,11 +1,9 @@
 package com.abtasty.demo;
 
-import com.abtasty.flagship.hits.Screen;
 import com.abtasty.flagship.main.Flagship;
 import com.abtasty.flagship.main.FlagshipConfig;
 import com.abtasty.flagship.utils.LogManager;
 import com.abtasty.flagship.visitor.Visitor;
-
 import java.util.HashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;

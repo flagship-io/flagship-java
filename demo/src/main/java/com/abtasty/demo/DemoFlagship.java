681eb081b20ea9be429167e514c073506c1cb6a6
= <raphael@abtasty.com>
2024-05-07 09:41:48 +0200

    conflicts resolution

--- demo/src/main/java/com/abtasty/demo/DemoFlagship.java
+++ demo/src/main/java/com/abtasty/demo/DemoFlagship.java
@@ -5,6 +5,7 @@ import com.abtasty.flagship.hits.Event;
 import com.abtasty.flagship.main.Flagship;
 import com.abtasty.flagship.main.FlagshipConfig;
 import com.abtasty.flagship.model.Flag;
+import com.abtasty.flagship.model.FlagMetadata;
 import com.abtasty.flagship.utils.LogManager;
 import com.abtasty.flagship.visitor.Visitor;
 import java.util.HashMap;
@@ -17,31 +18,34 @@ public class DemoFlagship {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
 
-        System.out.println("> " + System.getProperty("java.version"));
-        CountDownLatch flagshipReadyLatch = new CountDownLatch(1);
-        Flagship.start("_ENV_ID_", "_API_KEY_",
-                new FlagshipConfig.DecisionApi()
-                        .withLogLevel(LogManager.Level.ALL)
-                        .withStatusListener(newStatus -> {
-                            System.out.println("NEW STATUS = " + newStatus.name());
-                            if (newStatus.greaterThan(Flagship.Status.POLLING))
-                                flagshipReadyLatch.countDown();
-                        })
-//                        .withCacheManager(new SQLiteCacheManager())
-        );
+//        System.out.println("> " + System.getProperty("java.version"));
+//        CountDownLatch flagshipReadyLatch = new CountDownLatch(1);
+//        Flagship.start("_ENV_ID_", "_API_KEY_",
+//                new FlagshipConfig.DecisionApi()
+//                        .withLogLevel(LogManager.Level.ALL)
+//                        .withStatusListener(newStatus -> {
+//                            System.out.println("NEW STATUS = " + newStatus.name());
+//                            if (newStatus.greaterThan(Flagship.Status.POLLING))
+//                                flagshipReadyLatch.countDown();
+//                        })
+////                        .withCacheManager(new SQLiteCacheManager())
+//        );
+//
+//        flagshipReadyLatch.await();
+//        //
+//        Visitor visitor = Flagship.newVisitor("visitor_id")
+//                .context(new HashMap<String, Object>() {{
+//                    put("my_context", true);
+//                }}).build();
+//        visitor.fetchFlags().get();
+//        String value = visitor.getFlag("my_flag", "default").value(true);
+//        System.out.println("My flag value is : " + value);
+//        Thread.sleep(200);
+//        visitor.sendHit(new Screen("DemoFlagship.java"));
+//        visitor.sendHit(new Event(Event.EventCategory.USER_ENGAGEMENT, "action"));
+//        Thread.sleep(200);
 
-        flagshipReadyLatch.await();
-        Visitor visitor = Flagship.newVisitor("visitor_id")
-                .context(new HashMap<String, Object>() {{
-                    put("my_context", true);
-                }}).build();
-        visitor.fetchFlags().get();
-        String value = visitor.getFlag("my_flag", "default").value(true);
-        System.out.println("My flag value is : " + value);
-        Thread.sleep(200);
-        visitor.sendHit(new Screen("flz"));
-        visitor.sendHit(new Event(Event.EventCategory.USER_ENGAGEMENT, "action"));
-        Thread.sleep(200);
+
     }
 
 
